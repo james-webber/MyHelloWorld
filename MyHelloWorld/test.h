ff4eaa22e375c2d29b84545200c67292d40ced46
James Webber <jimwebber@gmail.com>
2012-12-14 14:12:03 +0000

    first commit

---
+++ MyHelloWorld/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  MyHelloWorld
+//
+//  Created by James Webber on 13/12/2012.
+//  Copyright (c) 2012 James Webber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ MyHelloWorld/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  MyHelloWorld
+//
+//  Created by James Webber on 13/12/2012.
+//  Copyright (c) 2012 James Webber. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
